063bff29322631f663d20223c0c8a9c7ae031159
Miguel26112001 <u202220294@upc.edu.pe>
2025-10-07 19:48:05 -0500

    style: align code formatting to Checkstyle rules for domain

--- src/main/java/com/hampcoders/electrolink/iam/domain/model/aggregates/User.java
+++ src/main/java/com/hampcoders/electrolink/iam/domain/model/aggregates/User.java
@@ -2,17 +2,23 @@ package com.hampcoders.electrolink.iam.domain.model.aggregates;
 
 import com.hampcoders.electrolink.iam.domain.model.entities.Role;
 import com.hampcoders.electrolink.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
- * User aggregate root
+ * User aggregate root.
  * This class represents the aggregate root for the User entity.
  *
  * @see AuditableAbstractAggregateRoot
@@ -32,27 +38,45 @@ public class User extends AuditableAbstractAggregateRoot<User> {
   private String password;
 
   @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-  @JoinTable(	name = "user_roles",
+  @JoinTable(name = "user_roles",
       joinColumns = @JoinColumn(name = "user_id"),
       inverseJoinColumns = @JoinColumn(name = "role_id"))
   private Set<Role> roles;
 
+  /**
+   * Default constructor for JPA.
+   */
   public User() {
     this.roles = new HashSet<>();
   }
+
+  /**
+   * Constructor with username and password.
+   *
+   * @param username the user's username
+   * @param password the user's encoded password
+   */
   public User(String username, String password) {
     this.username = username;
     this.password = password;
     this.roles = new HashSet<>();
   }
 
+  /**
+   * Constructor with username, password, and a list of roles.
+   *
+   * @param username the user's username
+   * @param password the user's encoded password
+   * @param roles the list of roles to assign to the user
+   */
   public User(String username, String password, List<Role> roles) {
     this(username, password);
     addRoles(roles);
   }
 
   /**
-   * Add a role to the user
+   * Add a role to the user.
+   *
    * @param role the role to add
    * @return the user with the added role
    */
@@ -62,7 +86,8 @@ public class User extends AuditableAbstractAggregateRoot<User> {
   }
 
   /**
-   * Add a list of roles to the user
+   * Add a list of roles to the user.
+   *
    * @param roles the list of roles to add
    * @return the user with the added roles
    */
