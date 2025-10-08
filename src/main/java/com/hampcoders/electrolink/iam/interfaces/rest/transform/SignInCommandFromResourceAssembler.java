867a6f66722906122a644ce04967cafca54a107d
Miguel26112001 <u202220294@upc.edu.pe>
2025-10-07 19:50:53 -0500

    style: align code formatting to Checkstyle rules for assemblers

--- src/main/java/com/hampcoders/electrolink/iam/interfaces/rest/transform/SignInCommandFromResourceAssembler.java
+++ src/main/java/com/hampcoders/electrolink/iam/interfaces/rest/transform/SignInCommandFromResourceAssembler.java
@@ -3,8 +3,17 @@ package com.hampcoders.electrolink.iam.interfaces.rest.transform;
 import com.hampcoders.electrolink.iam.domain.model.commands.SignInCommand;
 import com.hampcoders.electrolink.iam.interfaces.rest.resources.SignInResource;
 
+/**
+ * Assembler responsible for transforming a SignInResource into a SignInCommand.
+ */
 public class SignInCommandFromResourceAssembler {
 
+  /**
+   * Converts a SignInResource into a SignInCommand.
+   *
+   * @param signInResource The SignInResource.
+   * @return The SignInCommand.
+   */
   public static SignInCommand toCommandFromResource(SignInResource signInResource) {
     return new SignInCommand(signInResource.username(), signInResource.password());
   }
--- src/main/java/com/hampcoders/electrolink/iam/interfaces/rest/transform/SignUpCommandFromResourceAssembler.java
+++ src/main/java/com/hampcoders/electrolink/iam/interfaces/rest/transform/SignUpCommandFromResourceAssembler.java
@@ -4,20 +4,24 @@ import com.hampcoders.electrolink.iam.domain.model.commands.SignUpCommand;
 import com.hampcoders.electrolink.iam.domain.model.entities.Role;
 import com.hampcoders.electrolink.iam.interfaces.rest.resources.SignUpResource;
 
-import java.util.ArrayList;
-
+/**
+ * Assembler responsible for transforming a SignUpResource into a SignUpCommand.
+ */
 public class SignUpCommandFromResourceAssembler {
 
-    public static SignUpCommand toCommandFromResource(SignUpResource resource) {
-        // 1. Convertir los nombres de String a entidades temporales Role
-        var roles = resource.roles() != null
-                ? resource.roles().stream().map(name -> Role.toRoleFromName(name)).toList()
-                : null; // Dejar como 'null' para que validateRoleSet lo maneje.
+  /**
+   * Converts a SignUpResource into a SignUpCommand, applying default role logic if necessary.
+   *
+   * @param resource The SignUpResource.
+   * @return The resulting SignUpCommand.
+   */
+  public static SignUpCommand toCommandFromResource(SignUpResource resource) {
+    var roles = resource.roles() != null
+        ? resource.roles().stream().map(Role::toRoleFromName).toList()
+        : null;
 
-        // 2. Validar y obtener el rol por defecto si la lista es nula/vacía
-        var validatedRoles = Role.validateRoleSet(roles);
+    var validatedRoles = Role.validateRoleSet(roles);
 
-        // 3. Crear el comando con la lista de roles validada (o por defecto)
-        return new SignUpCommand(resource.username(), resource.password(), validatedRoles);
-    }
+    return new SignUpCommand(resource.username(), resource.password(), validatedRoles);
+  }
 }
