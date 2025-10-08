91ef3d9163ab18818f0f08c13218230ddb746b6e
Miguel26112001 <u202220294@upc.edu.pe>
2025-10-07 19:44:38 -0500

    style: align code formatting to Checkstyle rules for AuthenticatedUser resource and assembler

--- src/main/java/com/hampcoders/electrolink/iam/interfaces/rest/transform/AuthenticatedUserResourceFromEntityAssembler.java
+++ src/main/java/com/hampcoders/electrolink/iam/interfaces/rest/transform/AuthenticatedUserResourceFromEntityAssembler.java
@@ -3,8 +3,19 @@ package com.hampcoders.electrolink.iam.interfaces.rest.transform;
 import com.hampcoders.electrolink.iam.domain.model.aggregates.User;
 import com.hampcoders.electrolink.iam.interfaces.rest.resources.AuthenticatedUserResource;
 
+/**
+ * Assembler responsible for transforming a User entity
+ *     and a token into an AuthenticatedUserResource.
+ */
 public class AuthenticatedUserResourceFromEntityAssembler {
 
+  /**
+   * Converts a User entity and a token into an AuthenticatedUserResource.
+   *
+   * @param user The User entity.
+   * @param token The JWT token string.
+   * @return The AuthenticatedUserResource.
+   */
   public static AuthenticatedUserResource toResourceFromEntity(User user, String token) {
     return new AuthenticatedUserResource(user.getId(), user.getUsername(), token);
   }
