c08a593f870f282bcbbf9813c5e1c573fe7241aa
Miguel26112001 <u202220294@upc.edu.pe>
2025-10-07 19:45:37 -0500

    style: align code formatting to Checkstyle rules for BcryptHashingService

--- src/main/java/com/hampcoders/electrolink/iam/infrastructure/hashing/bcrypt/BcryptHashingService.java
+++ src/main/java/com/hampcoders/electrolink/iam/infrastructure/hashing/bcrypt/BcryptHashingService.java
@@ -6,7 +6,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 /**
  * This interface is a marker interface for the BCrypt hashing service.
  * It extends the {@link HashingService} and {@link PasswordEncoder} interfaces.
- * This interface is used to inject the BCrypt hashing service in the {@link HashingServiceImpl} class.
+ * This interface is used to inject the BCrypt
+ *     hashing service in the {@link HashingService} class.
  */
-public interface BCryptHashingService extends HashingService, PasswordEncoder {
+public interface BcryptHashingService extends HashingService, PasswordEncoder {
 }
