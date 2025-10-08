980fde7c66b0f4048886e1254f373d217980e432
Miguel26112001 <u202220294@upc.edu.pe>
2025-10-07 19:47:40 -0500

    style: align code formatting to Checkstyle rules for commands

--- src/main/java/com/hampcoders/electrolink/iam/domain/model/commands/SignUpCommand.java
+++ src/main/java/com/hampcoders/electrolink/iam/domain/model/commands/SignUpCommand.java
@@ -1,8 +1,14 @@
 package com.hampcoders.electrolink.iam.domain.model.commands;
 
 import com.hampcoders.electrolink.iam.domain.model.entities.Role;
-
 import java.util.List;
 
+/**
+ * Command to request user sign-up.
+ *
+ * @param username The desired username for the new user.
+ * @param password The raw password for the new user.
+ * @param roles The list of roles to assign to the new user.
+ */
 public record SignUpCommand(String username, String password, List<Role> roles) {
 }
