4e044d76d8a8264c0191c4c0fd8f6b5ecba50717
Miguel26112001 <u202220294@upc.edu.pe>
2025-10-08 22:58:52 -0500

    feat: modified domain commands

--- src/main/java/com/hampcoders/electrolink/monitoring/domain/model/commands/AddRatingCommand.java
+++ src/main/java/com/hampcoders/electrolink/monitoring/domain/model/commands/AddRatingCommand.java
@@ -1,12 +1,21 @@
 package com.hampcoders.electrolink.monitoring.domain.model.commands;
 
-import com.hampcoders.electrolink.monitoring.domain.model.valueObjects.RequestId;
-import com.hampcoders.electrolink.monitoring.domain.model.valueObjects.TechnicianId;
+import com.hampcoders.electrolink.monitoring.domain.model.valueobjects.RequestId;
+import com.hampcoders.electrolink.monitoring.domain.model.valueobjects.TechnicianId;
 
+/**
+ * Command to add a new rating for a service operation.
+ *
+ * @param requestId The ID of the service operation being rated.
+ * @param score The rating score (1-5).
+ * @param comment The optional comment.
+ * @param raterId The ID of the user submitting the rating.
+ * @param technicianId The ID of the technician who performed the service.
+ */
 public record AddRatingCommand(
-        RequestId requestId,
-        int score,
-        String comment,
-        String raterId,
-        TechnicianId technicianId
+    RequestId requestId,
+    int score,
+    String comment,
+    String raterId,
+    TechnicianId technicianId
 ) {}
