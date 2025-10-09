1fb66c53cf89cd55d02666d381f3383a4d370027
Leandro Saul Contreras Lopez <leandro25.cl@gmail.com>
2025-10-09 00:53:08 -0500

    feat: add detailed documentation (checkstyle) for repository interfaces in service design planning

--- src/main/java/com/hampcoders/electrolink/sdp/infrastructure/persistence/jpa/repositories/ServiceRepository.java
+++ src/main/java/com/hampcoders/electrolink/sdp/infrastructure/persistence/jpa/repositories/ServiceRepository.java
@@ -4,6 +4,10 @@ import com.hampcoders.electrolink.sdp.domain.model.aggregates.ServiceEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface for managing ServiceEntity entities.
+ * Extends JpaRepository to provide CRUD operations and custom query methods.
+ */
 @Repository
 public interface ServiceRepository extends JpaRepository<ServiceEntity, Long> {
 }
