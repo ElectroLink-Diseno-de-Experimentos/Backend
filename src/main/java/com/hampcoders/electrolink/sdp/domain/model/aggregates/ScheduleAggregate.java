def3f55f248dc313b72c1ed797c98e4cec1ba559
Leandro Saul Contreras Lopez <leandro25.cl@gmail.com>
2025-10-09 00:55:38 -0500

    feat: enhance Request, ScheduleAggregate, and ServiceEntity classes with detailed constructors and documentation (checkstyle)

--- src/main/java/com/hampcoders/electrolink/sdp/domain/model/aggregates/ScheduleAggregate.java
+++ src/main/java/com/hampcoders/electrolink/sdp/domain/model/aggregates/ScheduleAggregate.java
@@ -1,31 +1,56 @@
 package com.hampcoders.electrolink.sdp.domain.model.aggregates;
 
 import com.hampcoders.electrolink.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * Represents a technician's schedule as an aggregate root.
+ */
 @Entity
 @Getter
 @NoArgsConstructor
 public class ScheduleAggregate extends AuditableAbstractAggregateRoot<ScheduleAggregate> {
 
-    private String technicianId;
+  private String technicianId;
 
-    private String day;
-    private String startTime;
-    private String endTime;
+  private String day;
+  private String startTime;
+  private String endTime;
 
-    public ScheduleAggregate(String technicianId, String day, String startTime, String endTime) {
-        this.technicianId = technicianId;
-        this.day = day;
-        this.startTime = startTime;
-        this.endTime = endTime;
-    }
+  /**
+   * Constructs a new ScheduleAggregate.
+   *
+   * @param technicianId The ID of the technician.
+   *
+   * @param day          The day of the week for the schedule entry.
+   *
+   * @param startTime    The start time of the availability.
+   *
+   * @param endTime      The end time of the availability.
+   *
+   */
+  public ScheduleAggregate(String technicianId, String day, String startTime, String endTime) {
+    this.technicianId = technicianId;
+    this.day = day;
+    this.startTime = startTime;
+    this.endTime = endTime;
+  }
 
-    public void updateFrom(String day, String startTime, String endTime) {
-        this.day = day;
-        this.startTime = startTime;
-        this.endTime = endTime;
-    }
+  /**
+   * Updates the schedule's data.
+   *
+   * @param day       The new day of the week.
+   *
+   * @param startTime The new start time.
+   *
+   * @param endTime   The new end time.
+   *
+   */
+  public void updateFrom(String day, String startTime, String endTime) {
+    this.day = day;
+    this.startTime = startTime;
+    this.endTime = endTime;
+  }
 }
