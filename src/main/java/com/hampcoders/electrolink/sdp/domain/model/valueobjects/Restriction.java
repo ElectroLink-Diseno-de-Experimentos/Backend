c8e8eed37e5008a870428a47b5fdb2b46e41fbd6
Leandro Saul Contreras Lopez <leandro25.cl@gmail.com>
2025-10-09 00:51:15 -0500

    feat: enhance Policy and Restriction classes with detailed constructors and documentation (checkstyle)

--- src/main/java/com/hampcoders/electrolink/sdp/domain/model/valueobjects/Restriction.java
+++ src/main/java/com/hampcoders/electrolink/sdp/domain/model/valueobjects/Restriction.java
@@ -2,27 +2,39 @@ package com.hampcoders.electrolink.sdp.domain.model.valueobjects;
 
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Embeddable;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
-
+/**
+ * Represents restrictions for a service or technician.
+ * It is an embeddable value object.
+ */
 @Embeddable
 @Getter
 @NoArgsConstructor
 public class Restriction {
-    @ElementCollection
-    private List<String> unavailableDistricts = new ArrayList<>();
+  @ElementCollection
+  private List<String> unavailableDistricts = new ArrayList<>();
 
-    @ElementCollection
-    private List<String> forbiddenDays = new ArrayList<>();
+  @ElementCollection
+  private List<String> forbiddenDays = new ArrayList<>();
 
-    private boolean requiresSpecialCertification;
+  private boolean requiresSpecialCertification;
 
-    public Restriction(List<String> unavailableDistricts, List<String> forbiddenDays, boolean requiresSpecialCertification) {
-        this.unavailableDistricts = unavailableDistricts;
-        this.forbiddenDays = forbiddenDays;
-        this.requiresSpecialCertification = requiresSpecialCertification;
-    }
+  /**
+   * Constructor for Restriction.
+   *
+   * @param unavailableDistricts List of unavailable districts.
+   * @param forbiddenDays List of forbidden days.
+   * @param requiresSpecialCertification Whether special certification is required.
+   */
+  public Restriction(List<String> unavailableDistricts,
+                     List<String> forbiddenDays,
+                     boolean requiresSpecialCertification) {
+    this.unavailableDistricts = unavailableDistricts;
+    this.forbiddenDays = forbiddenDays;
+    this.requiresSpecialCertification = requiresSpecialCertification;
+  }
 }
