4abe81d0449303171fa3482a2aedad77807b5163
Leandro Saul Contreras Lopez <leandro25.cl@gmail.com>
2025-10-09 00:50:12 -0500

    feat: add detailed documentation (checkstyle) for query classes in the service design planning

--- src/main/java/com/hampcoders/electrolink/sdp/domain/model/queries/FindRequestByIdQuery.java
+++ src/main/java/com/hampcoders/electrolink/sdp/domain/model/queries/FindRequestByIdQuery.java
@@ -1,3 +1,9 @@
 package com.hampcoders.electrolink.sdp.domain.model.queries;
 
+/**
+ * Represents a query to find a request by its ID.
+ *
+ * @param requestId The ID of the request to find.
+ *
+ */
 public record FindRequestByIdQuery(Long requestId) {}
