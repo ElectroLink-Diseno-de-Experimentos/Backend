9fa236d3ff9af411fed3dca07c6c9611b828a192
Leandro Saul Contreras Lopez <leandro25.cl@gmail.com>
2025-10-09 00:49:13 -0500

    feat: add detailed documentation (checkstyle) for resource classes in the service design planning

--- src/main/java/com/hampcoders/electrolink/sdp/interfaces/rest/resources/RestrictionResource.java
+++ src/main/java/com/hampcoders/electrolink/sdp/interfaces/rest/resources/RestrictionResource.java
@@ -2,8 +2,17 @@ package com.hampcoders.electrolink.sdp.interfaces.rest.resources;
 
 import java.util.List;
 
+/**
+ * Resource representing restrictions.
+ *
+ * @param unavailableDistricts List of unavailable districts.
+ * @param forbiddenDays List of forbidden days.
+ * @param requiresSpecialCertification Whether special certification is required.
+ */
 public record RestrictionResource(
-        List<String> unavailableDistricts,
-        List<String> forbiddenDays,
-        boolean requiresSpecialCertification
-) {}
+    List<String> unavailableDistricts,
+    List<String> forbiddenDays,
+    boolean requiresSpecialCertification
+) {
+
+}
